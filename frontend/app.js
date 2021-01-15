5d27320fcbf37a37dff40b5a3af62038b795103d
rearalf <rear_recinos@hotmail.com>
2021-01-15 15:37:23 -0600

    Mejora en el diseno.

--- frontend/app.js
+++ frontend/app.js
@@ -8,13 +8,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.getElementById('bookForm').addEventListener('submit', e => {
 	e.preventDefault();
-
 	// Inputs value
 	const title = e.target[0].value;
 	const author = e.target[1].value;
 	const view = e.target[2].value;
-	const cover = e.target[3].files;
-
+	const cover = e.target[3].files[0];
 	ui.addNewBook({ title, author, view, cover });
 });
 
@@ -25,3 +23,19 @@ document.getElementById('ListBooks').addEventListener('click', e => {
 		ui.deleteBook({ id });
 	}
 });
+
+document.querySelector('#inputImgForm').addEventListener('change', e => {
+	const imgForm = document.getElementById('imgForm');
+	const curFiles = e.target.files;
+	for (const file of curFiles) {
+		if (validFileType(file)) {
+			imgForm.src = URL.createObjectURL(file);
+		}
+	}
+});
+
+const fileTypes = [ 'image/apng', 'image/jpeg', 'image/pjpeg', 'image/png', 'image/webp' ];
+
+function validFileType(file){
+	return fileTypes.includes(file.type);
+}
